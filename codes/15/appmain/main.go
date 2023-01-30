07204343766ec5407de6457492dea7f85f8cd38e
daniel-zhai <daniel_zhai@trendmicro.com>
2023-01-30 17:33:39 +0800

    done 15

--- codes/15/appmain/main.go
+++ codes/15/appmain/main.go
@@ -4,10 +4,22 @@ import (
 
 	// "github.com/zcmyron/learn-go-basic/codes/10/submodels"
 
+	"fmt"
+	"time"
+
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/zcmyron/learn-go-basic/codes/10/services/users"
 )
 
-func main() {
+func sum(max int) {
+	result := 0
+	for i := 0; i <= max; i++ {
+		result += i
+	}
+	fmt.Println(result)
+}
 
+func main() {
+	defer time.Sleep(time.Second * 3)
+	go sum(100)
 }
