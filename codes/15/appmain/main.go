8f47b862c8d8e80249cca1f73d788405eacf3fc1
daniel-zhai <daniel_zhai@trendmicro.com>
2023-01-31 18:09:33 +0800

    16 done

--- codes/15/appmain/main.go
+++ codes/15/appmain/main.go
@@ -11,15 +11,24 @@ import (
 	_ "github.com/zcmyron/learn-go-basic/codes/10/services/users"
 )
 
-func sum(max int) {
+func sum(min int, max int, c chan int) {
 	result := 0
-	for i := 0; i <= max; i++ {
+	for i := min; i <= max; i++ {
 		result += i
 	}
-	fmt.Println(result)
+	c <- result
 }
 
 func main() {
-	defer time.Sleep(time.Second * 3)
-	go sum(100)
+	num := 1000001
+	min := 0
+	c := make(chan int)
+	start := time.Now()
+	go sum(min, num/4, c)
+	go sum(num/4+1, num/2, c)
+	go sum(num/2+1, num*3/4, c)
+	go sum(num*3/4+1, num, c)
+	res1, res2, res3, res4 := <-c, <-c, <-c, <-c
+	end := time.Now()
+	fmt.Println(end.Sub(start), res1+res2+res3+res4)
 }
