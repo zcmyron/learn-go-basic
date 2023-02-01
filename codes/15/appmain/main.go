77bd7530574f294af2cc9e2b9e1d080c722e848f
daniel-zhai <daniel_zhai@trendmicro.com>
2023-02-01 10:54:47 +0800

    done 17

--- codes/15/appmain/main.go
+++ codes/15/appmain/main.go
@@ -5,30 +5,55 @@ import (
 	// "github.com/zcmyron/learn-go-basic/codes/10/submodels"
 
 	"fmt"
-	"time"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/zcmyron/learn-go-basic/codes/10/services/users"
 )
 
-func sum(min int, max int, c chan int) {
-	result := 0
-	for i := min; i <= max; i++ {
-		result += i
-	}
-	c <- result
-}
+// func sum(min int, max int, c chan int) {
+// 	result := 0
+// 	for i := min; i <= max; i++ {
+// 		result += i
+// 	}
+// 	c <- result
+// }
 
 func main() {
-	num := 1000001
-	min := 0
-	c := make(chan int)
-	start := time.Now()
-	go sum(min, num/4, c)
-	go sum(num/4+1, num/2, c)
-	go sum(num/2+1, num*3/4, c)
-	go sum(num*3/4+1, num, c)
-	res1, res2, res3, res4 := <-c, <-c, <-c, <-c
-	end := time.Now()
-	fmt.Println(end.Sub(start), res1+res2+res3+res4)
+	// num := 1000001
+	// min := 0
+	// c := make(chan int)
+	// start := time.Now()
+	// go sum(min, num/4, c)
+	// go sum(num/4+1, num/2, c)
+	// go sum(num/2+1, num*3/4, c)
+	// go sum(num*3/4+1, num, c)
+	// res1, res2, res3, res4 := <-c, <-c, <-c, <-c
+	// end := time.Now()
+	// fmt.Println(end.Sub(start), res1+res2+res3+res4)
+	users := strings.Split("aa,bb,cc,dd,ee", ",")
+	ages := strings.Split("1,2,3,4,5", ",")
+
+	c1, c2 := make(chan bool), make(chan bool)
+	ret := make([]string, 0)
+
+	go func() {
+		for _, v := range users {
+			<-c1
+			ret = append(ret, v)
+			c2 <- true
+		}
+	}()
+	go func() {
+		for _, v := range ages {
+			<-c2
+			ret = append(ret, v)
+			c1 <- true
+		}
+
+	}()
+
+	c1 <- true
+
+	fmt.Println(ret)
 }
