c278461a21d4cbb321395d35bc8cdb3bb3ab6f3f
daniel-zhai <daniel_zhai@trendmicro.com>
2023-02-13 17:54:06 +0800

    21 done

--- codes/18/appmain/main.go
+++ codes/18/appmain/main.go
@@ -5,12 +5,12 @@ import (
 	// "github.com/zcmyron/learn-go-basic/codes/10/submodels"
 
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"time"
-
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/zcmyron/learn-go-basic/codes/10/services/users"
+	"io/ioutil"
+	"net/http"
+	"runtime"
+	"sync"
 )
 
 func test() (ret int) {
@@ -23,14 +23,15 @@ func test() (ret int) {
 func main() {
 	url := "https://www.cnblogs.com/#p%d"
 
-	c := make(chan map[int][]byte)
-
-	for i := 0; i <= 3; i++ {
+	//c := make(chan map[int][]byte)
+	wg := sync.WaitGroup{}
+	for i := 0; i <= 10; i++ {
 		go func(index int) {
 			defer func() {
 				if err := recover(); err != nil {
 					fmt.Println(err)
 				}
+				wg.Done()
 
 				// if index == 3 {
 				// 	close(c)
@@ -39,10 +40,13 @@ func main() {
 			url := fmt.Sprintf(url, index)
 			res, _ := http.Get(url)
 			cnt, _ := ioutil.ReadAll(res.Body)
-			c <- map[int][]byte{index: cnt}
+			//c <- map[int][]byte{index: cnt}
+			ioutil.WriteFile(fmt.Sprintf("../files/%d", index), cnt, 0777)
 		}(i)
-
+		wg.Add(1)
 	}
+	fmt.Println("number of goroutines: ", runtime.NumGoroutine())
+	wg.Wait()
 	// i := 0
 	// for getcnt := range c {
 	// 	for k, v := range getcnt {
@@ -53,19 +57,19 @@ func main() {
 	// 		close(c)
 	// 	}
 	// }
-
-	result := map[int][]byte{}
-myloop:
-	for {
-		select {
-		case result = <-c:
-			for k, v := range result {
-				ioutil.WriteFile(fmt.Sprintf("../files/%d", k), v, 0777)
-			}
-		case <-time.After(time.Second * 3):
-			break myloop
-		}
-	}
+	//
+	//	result := map[int][]byte{}
+	//myloop:
+	//	for {
+	//		select {
+	//		case result = <-c:
+	//			for k, v := range result {
+	//				ioutil.WriteFile(fmt.Sprintf("../files/%d", k), v, 0777)
+	//			}
+	//		case <-time.After(time.Second * 3):
+	//			break myloop
+	//		}
+	//	}
 
 	// defer func() {
 	// 	err := recover()
@@ -77,3 +81,20 @@ myloop:
 	// fmt.Println(test())
 
 }
+
+//func main() {
+//	var wg = sync.WaitGroup{}
+//	for i := 0; i <= 5; i++ {
+//		go func(index int) {
+//			defer func() {
+//				wg.Done()
+//			}()
+//			fmt.Println(index, "Done")
+//		}(i)
+//		wg.Add(1)
+//	}
+//	fmt.Println("number of goroutine: ", runtime.NumGoroutine())
+//	wg.Wait()
+//	fmt.Println("all done!")
+//
+//}
